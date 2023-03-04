205767c9b16c7fb11168bbb5df5e5a5841a70748
lincolnmuller <thyssenkrupp234@gmail.com>
2023-03-03 19:14:13 -0800

    add verison check to make sure os supports MDC

--- src/jbbtn.ts
+++ src/jbbtn.ts
@@ -1,20 +1,33 @@
 const { exec } = require('child_process');
 const btn = document.querySelector("button") as HTMLButtonElement;
 const path = require("path");
+const os = require('os');
+exec('sw_vers -productVersion', (error: any, stdout: string, stderr: any) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
 
-document.addEventListener('DOMContentLoaded', () => {
+    const osversion = stdout.trim();
+    console.log("macOS version is "+osversion);
+    if (osversion === "12.6.2" || osversion === "12.6.3" || osversion === "13.1" || osversion === "13.2" || osversion == "13.2.1") {
+        btn.innerText = "unsupported";
+        btn.style.backgroundColor = "#FF0000";
+        btn.disabled = true;
+        return;
+    }
     btn.onclick = () => {
         btn.textContent = "jailbreaking";
         btn.disabled = true;
         btn.style.backgroundColor = "#FFC300";
         exec(path.join(__dirname,"/resources/exploit/switcharoo"+" /etc/pam.d/su "+path.join(__dirname,"/resources/exploit/overwrite_file.bin")), (error: any, stdout: any, stderr: any) => {
             if (error) {
-              console.error(`exec error: ${error}`);
-              return;
+                console.error(`exec error: ${error}`);
+                return;
             }
             console.log(`stdout: ${stdout}`);
             console.error(`stderr: ${stderr}`);
             btn.style.backgroundColor = "#32e000";
-          });
+        });
     };
 });
